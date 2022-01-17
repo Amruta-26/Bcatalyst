b01c19d39d78afbd45f54b6befdeb83e580d2aa8
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2022-01-17 18:47:31 +0530

    form

--- GetPost/server.js
+++ GetPost/server.js
@@ -6,17 +6,20 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/test/:name", function(req, res) {
-  res.send("Hello "+req.params.name + " !! Welcome to NodeJS App");
+app.use(express.static(__dirname + "/public"));
+app.get("/", () => {
+    res.sendfile(__dirname + "/public/index.html");
+});
+app.get("/test/:name", function (req, res) {
+    res.send("Hello " + req.params.name + " !! Welcome to NodeJS App");
 });
 
-
-app.post("/test", function(req, res) {
-  console.log(req.body);
-  const username = req.body.username;
-  res.send("Post Route Hit by : " + username);
+app.post("/test", function (req, res) {
+    console.log(req.body);
+    const username = req.body.username;
+    res.send("Post Route Hit by : " + username);
 });
 
-app.listen(3000, function() {
-  console.log("Server is up and running at port 3000");
+app.listen(3000, function () {
+    console.log("Server is up and running at port 3000");
 });
