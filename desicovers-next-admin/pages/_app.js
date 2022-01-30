f14445a674cd4143fd92615965172f4399ce99a1
Hrithik Tiwari <javaoncloud14@gmail.com>
2022-01-30 20:06:20 +0530

    Removed bgimage & font from css files

--- desicovers-next-admin/pages/_app.js
+++ desicovers-next-admin/pages/_app.js
@@ -1,6 +1,17 @@
 import "../styles/globals.css";
-import "../styles/css/*.css";
+// import "../styles/css/";
 import Sidebar from "./components/sidebar";
+import "../styles/css/autocomplete.css";
+import "../styles/css/changelists.css";
+import "../styles/css/fonts.css";
+import "../styles/css/login.css";
+import "../styles/css/responsive_rtl.css";
+import "../styles/css/base.css";
+import "../styles/css/dashboard.css";
+import "../styles/css/forms.css";
+import "../styles/css/responsive.css";
+import "../styles/css/rtl.css";
+import "../styles/css/widgets.css";
 
 function MyApp({ Component, pageProps }) {
     return (
