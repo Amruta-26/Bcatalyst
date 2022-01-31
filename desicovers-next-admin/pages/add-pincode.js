f53b7af07f3ebdaf18a5203a54b7284053b50c87
Amruta-26 <darakamruta@gmail.com>
2022-01-31 10:03:03 +0530

    Added custtomer related js files

---
+++ desicovers-next-admin/pages/add-pincode.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Link from "next/link";
+import Sidebar from "./components/sidebar";
+
+export default function Home() {
+    return (
+        <>
+            <h1><center>Add Pincode</center></h1>
+
+            <form action="/pincode/" method="POST" encype="multipart/form-data" className="form-horizontal">
+
+
+                <div className='form-group'>
+
+                    <ul className="messages">
+
+                        <div className="alert alert-error" style='color: red'></div>
+                    </ul>
+
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="name" className="col-md-3 col-sm-3 col-xs-12 control-label">File: </label>
+                    <div className="col-md-8">
+                        <input style="height: 45px;" type="file" name="csv_file" id="csv_file" required="True" className="form-control" />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="col-md-3 col-sm-3 col-xs-12 col-md-offset-3" style="margin-bottom:10px;">
+                        <button className="btn btn-primary"> <span className="glyphicon glyphicon-upload" style="margin-right:5px;"></span>Upload </button>
+                    </div>
+                </div>
+            </form>
+
+            <table id="gallery" className="table table-bordered" style="margin-left:10px; width: 98%;">
+                <thead>
+                    <tr>
+                        <th>Pincode</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                    <tr>
+                        <td></td>
+                        <td><a href="/destroypincode/" className='btn btn-danger'>Delete</a></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </>
+    );
+}
